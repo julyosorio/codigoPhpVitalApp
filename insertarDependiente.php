f132524c659dad7dc99cfa738be2f42e53c4d93c
Julieth Osorio Tafur <ljosoriot@gmail.com>
2017-06-10 22:39:23 -0500

    Update insertarDependiente.php

--- insertarDependiente.php
+++ insertarDependiente.php
@@ -8,12 +8,12 @@ $eps=$_GET['eps'];
 $correo=$_GET['correo'];
 $telefono=$_GET['telefono'];
 $direccion=$_GET['direccion'];
-$usuariofk=$_GET['usuariofk'];
+$condicion=$_GET['condicion'];
+$enfermedad=$_GET['enfermedad'];
+$medicamentos=$_GET['medicamentos'];
 
-ejecutarSQLCommand("INSERT INTO `dependiente` (identificacion, nombre, fecha, tipo, eps, 
-	correo, telefono, direccion, usuariofk)
-VALUES ( '$identificacion', '$nombre', '$fecha', '$tipo', '$eps', '$correo', 
-	'$telefono','$direccion','$usuariofk')
+ejecutarSQLCommand("INSERT INTO `dependiente` (`identificacion`, `nombre`, `fecha`, `tipo`, `eps`, `correo`, `telefono`, `direccion`, `condicion`, `enfermedad`, `medicamentos`)
+VALUES ('$identificacion','$nombre','$fecha','$tipo','$eps','$correo','$telefono','$direccion','$condicion','$enfermedad', '$medicamentos')
 
  ON DUPLICATE KEY UPDATE `nombre`= '$nombre';");
 
